a2ccbb25d7ceabf553cf25bc4afd414d1d890394
Michael Neely <michael@michaelneely.net>
2024-05-17 18:00:43 -0400

    It works

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '32d4b1f3d7075cd1b2d8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0e1a8dd4345a27dd3397');
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
 import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+import { useBlockProps, RichText, MediaUpload, InspectorControls } from '@wordpress/block-editor';
+import { Button, PanelBody } from '@wordpress/components';
+import './style.scss';
+import './editor.scss';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -37,3 +42,105 @@ registerBlockType( metadata.name, {
 	 */
 	save,
 } );
+
+registerBlockType('create-block/floating-cta', {
+	title: __('Floating CTA', 'floating-cta'),
+	icon: 'megaphone',
+	category: 'widgets',
+	attributes: {
+		heading: {
+			type: 'string',
+			source: 'html',
+			selector: 'h2',
+		},
+		imageUrl: {
+			type: 'string',
+			default: '',
+		},
+		buttonText: {
+			type: 'string',
+			default: 'Sign Up',
+		},
+		buttonUrl: {
+			type: 'string',
+			default: '#',
+		},
+	},
+	edit({ attributes, setAttributes }) {
+		const blockProps = useBlockProps();
+
+		const onChangeHeading = (newHeading) => {
+			setAttributes({ heading: newHeading });
+		};
+
+		const onSelectImage = (media) => {
+			setAttributes({ imageUrl: media.url });
+		};
+
+		const onChangeButtonText = (newButtonText) => {
+			setAttributes({ buttonText: newButtonText });
+		};
+
+		const onChangeButtonUrl = (newButtonUrl) => {
+			setAttributes({ buttonUrl: newButtonUrl });
+		};
+
+		return (
+			<div {...blockProps}>
+				<InspectorControls>
+					<PanelBody title={__('Image Settings', 'floating-cta')}>
+						<MediaUpload
+							onSelect={onSelectImage}
+							allowedTypes={['image']}
+							render={({ open }) => (
+								<Button onClick={open} isDefault isLarge>
+									{__('Select Image', 'floating-cta')}
+								</Button>
+							)}
+						/>
+					</PanelBody>
+				</InspectorControls>
+				<RichText
+					tagName="h2"
+					value={attributes.heading}
+					onChange={onChangeHeading}
+					placeholder={__('Heading...', 'floating-cta')}
+				/>
+				{attributes.imageUrl && (
+					<img src={attributes.imageUrl} alt={__('Selected Image', 'floating-cta')} />
+				)}
+				<RichText
+					tagName="a"
+					className="cta-button"
+					value={attributes.buttonText}
+					onChange={onChangeButtonText}
+					placeholder={__('Button Text...', 'floating-cta')}
+				/>
+				<RichText
+					tagName="a"
+					className="cta-button-url"
+					value={attributes.buttonUrl}
+					onChange={onChangeButtonUrl}
+					placeholder={__('Button URL...', 'floating-cta')}
+				/>
+				<button className="cta-close-button">X</button>
+			</div>
+		);
+	},
+	save({ attributes }) {
+		const blockProps = useBlockProps.save();
+
+		return (
+			<div {...blockProps}>
+				<RichText.Content tagName="h2" value={attributes.heading} />
+				{attributes.imageUrl && (
+					<img src={attributes.imageUrl} alt={__('Selected Image', 'floating-cta')} />
+				)}
+				<a href={attributes.buttonUrl} className="cta-button">
+					<RichText.Content value={attributes.buttonText} />
+				</a>
+				<button className="cta-close-button">X</button>
+			</div>
+		);
+	},
+});
